7c9f4cb09251fbcd8c793f83ff2bce9a26a32e46
ana <111012665+aanamartinss@users.noreply.github.com>
2022-09-19 14:34:02 -0300

    Add files via upload

---
+++ taf.py
@@ -0,0 +1,26 @@
+class funcionario:
+  def __init__():
+  nome=[]
+  departamento=[]
+  valor=[]
+  salario=[]
+  rg=[]
+  datadm=[]
+  cont=1
+  cont=int(input("quantos funcionarios seram cadratrados :"))
+  while i < cont:
+    nom[input]("nome do funcionario:")
+    nome.append(nome)
+    dep-input("qual departamento",(nome),"trabalha")
+    departamento.append(dep)
+    valor-float(input("qual salario do funcionario",(nome),))
+    salario.append(valor)
+    data-input("qual a data de admissao do funcionario",(nome),)
+    datadm.append(data)
+    rg-input("qual o rg do funcionario",(nome),)
+    rg.append(rg)
+    valor+= 1.15%
+    print(valor)
+    print()
+    
+    
